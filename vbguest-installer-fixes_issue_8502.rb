e567e4385525f65f77c6b8afdfb20ffc13c183a7
Stefanos Kalantzis <stefanos.kalantzis@iconplc.com>
2017-05-25 10:44:18 +0100

    Add fix for https://github.com/mitchellh/vagrant/issues/8502

--- vbguest-installer-fixes_issue_8502.rb
+++ vbguest-installer-fixes_issue_8502.rb
@@ -3,9 +3,9 @@ class Installer8502 < VagrantVbguest::Installers::Ubuntu
   def install(opts=nil, &block)
     super
     cmd =   'if [ ! -e /sbin/mount.vboxsf ]; then'\
-	    '  v=$(/usr/bin/VBoxControl -V | cut -d"r" -f1);'\
-	    '  ln -sf /opt/VBoxGuestAdditions-${v}/lib/VBoxGuestAdditions/mount.vboxsf /sbin/mount.vboxsf;'\
-	    'fi'
+            '  v=$(/usr/bin/VBoxControl -V | cut -d"r" -f1);'\
+            '  ln -sf /opt/VBoxGuestAdditions-${v}/lib/VBoxGuestAdditions/mount.vboxsf /sbin/mount.vboxsf;'\
+            'fi'
     communicate.sudo(cmd, nil, &block)
   end
 end
